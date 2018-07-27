2511b81c3fb602ceef19e62148e3bd59ad27bbf7
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-27 11:13:12 +0200

    exo6p4 php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*Déclaration de la fonction*/
+  function identity($lastName, $firstName, $age){
+    /*Paramétrage de l'affichage*/
+    echo 'bonjour ' . $firstName . $lastName . ', tu as ' . $age . ' ans';
+  }
+  /*Nommage des valeurs de variables paramètres*/
+  identity(' Lamotte ', ' Fabien ', 26);
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 6</title>
+  </head>
+  <body>
+    <p>
+    </p>
+  </body>
+</html>
